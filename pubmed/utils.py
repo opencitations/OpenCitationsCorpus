a60cdc68560ed0482a0ca9afeaf1334574627970
Alexander Dutton <alexander.dutton@zoo.ox.ac.uk>
2011-04-27 11:32:59 +0100

    Parameterised get_graphs with input_directory.

--- pubmed/utils.py
+++ pubmed/utils.py
@@ -1,15 +1,21 @@
-import os, stat, traceback, sys
+import os, stat, traceback, sys, time
+from datetime import datetime
 from lxml import etree
 
-def get_graphs():
+def get_graphs(input_directory):
     #directories = sorted(os.listdir('out'), key=lambda x:max([0]+[os.stat(os.path.join('out', x, y))[stat.ST_MTIME] for y in os.listdir(os.path.join('out', x))]), reverse=True)
-    directories = os.listdir('../out')
+    directories = os.listdir(input_directory)
+    directories.sort()
     for directory in directories:
-        for filename in os.listdir(os.path.join('..', 'out', directory)):
-            filename = os.path.join('..', 'out', directory, filename)
+        if not os.path.isdir(os.path.join(input_directory, directory)):
+            continue
+        for filename in os.listdir(os.path.join(input_directory, directory)):
+            filename = os.path.join(input_directory, directory, filename)
             if os.stat(filename).st_size > 10e6:
                 print "Skipping %r" % filename
                 continue
+            #if datetime.fromtimestamp(os.stat(filename).st_mtime) < datetime(2011, 03, 02, 11, 0, 0):
+            #    break
             if filename.endswith('.xml'):
                 try:
                     with open(filename) as f:
