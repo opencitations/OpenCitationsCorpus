8ec6c0b1ed6dd652b19474192fef6fd3851b2703
Alexander Dutton <alexander.dutton@zoo.ox.ac.uk>
2011-04-27 10:23:56 +0100

    Updated model to include more fields pulled out by article-data.xsl.

--- pubmed/model.py
+++ pubmed/model.py
@@ -3,7 +3,9 @@ from collections import namedtuple
 article_fields = (
     "group id xml_container ctype journal doi pmid pmc isbn full_citation year "
   + "month day title author source license publisher issue volume edition "
-  + "fpage lpage keywords abstract filename").split()
+  + "fpage lpage keywords abstract filename in_text_reference_pointer_count "
+  + "reference_count uri in_oa_subset retracted patent_number").split()
+
 article_field_set = set(article_fields)
 Article = namedtuple('Article', article_fields)
 
