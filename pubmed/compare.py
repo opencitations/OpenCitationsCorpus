6fb43c45a4c44b6deaee51246081e6ff554ec93a
Alexander Dutton <alexander.dutton@zoo.ox.ac.uk>
2011-04-27 11:23:46 +0100

    Added pmc and uri to distance metric.

--- pubmed/compare.py
+++ pubmed/compare.py
@@ -1,13 +1,14 @@
+from __future__ import division
 import re, itertools
 
 from Levenshtein import distance as levenshtein_distance
 
 def levenshtein(a, b):
-        d = levenshtein_distance(a, b) / max(len(a), len(b))
-        return (1 - d)
+    d = levenshtein_distance(a, b) / max(len(a), len(b))
+    return (1 - d)
 
 def compare_authors(a, b):
-    a_names, b_names = a.decode('utf-8').replace(u'.', u'').split(u', '), b.decode('utf-8').replace(u'.', u'').split(u', ')
+    a_names, b_names = a.replace(u'.', u'').split(u', '), b.replace(u'.', u'').split(u', ')
 
     cs = []
 
@@ -47,6 +48,7 @@ def compare_authors(a, b):
 
 INTEGER_RE = re.compile('^\d+')
 def compare_integer(a, b, weight=1):
+    #print "A", type(a), type(b)
     a = INTEGER_RE.match(a)
     b = INTEGER_RE.match(b)
     if a and b:
@@ -62,16 +64,21 @@ def compare(a, b):
         cs.append((0.4, 1 if a.doi == b.doi else 0))
     if a.pmid and b.pmid:
         cs.append((0.4, 1 if a.pmid == b.pmid else 0))
+    if a.pmc and b.pmc:
+        cs.append((0.4, 1 if a.pmc == b.pmc else 0))
+    if a.uri and b.uri:
+        cs.append((0.4, 1 if a.uri == b.uri else 0))
     if a.volume and b.volume:
         cs.append(compare_integer(a.volume, b.volume, weight=0.4))
     if a.issue and b.issue:
         cs.append(compare_integer(a.issue, b.issue, weight=0.4))
     if a.source and b.source:
-        cs.append((0.4, levenshtein(a.source.decode('utf-8').replace('.', '').lower(), b.source.decode('utf-8').replace('.', '').lower())))
+        cs.append((0.4, levenshtein(a.source.replace('.', '').lower(), b.source.replace('.', '').lower())))
     if a.title and b.title:
-        cs.append((1, levenshtein(a.title.decode('utf-8').lower(), b.title.decode('utf-8').lower())))
+        cs.append((1, levenshtein(a.title.lower(), b.title.lower())))
     if a.author and b.author:
         cs.append(compare_authors(a.author, b.author))
+
     if a.year and b.year:
         try:
             if int(a.year) < 1800 or int(b.year) < 1800:
