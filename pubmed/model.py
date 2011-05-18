93a1bd0e2c76c3a8c07aebb17338e87f3a8a132f
Alexander Dutton <alexander.dutton@zoo.ox.ac.uk>
2011-05-18 15:14:18 +0100

    Added more fields to the model that are being pulled out of the NLM XML.

--- pubmed/model.py
+++ pubmed/model.py
@@ -1,10 +1,10 @@
 from collections import namedtuple
 
 article_fields = (
-    "group id xml_container ctype journal doi pmid pmc isbn full_citation year "
+    "group id xml_container ctype journal doi pmid pmc isbn pii  nihmsid full_citation year "
   + "month day title author source license publisher issue volume edition "
   + "fpage lpage keywords abstract filename in_text_reference_pointer_count "
-  + "reference_count uri in_oa_subset retracted patent_number").split()
+  + "reference_count uri in_oa_subset retracted patent_number provenance").split()
 
 article_field_set = set(article_fields)
 Article = namedtuple('Article', article_fields)
