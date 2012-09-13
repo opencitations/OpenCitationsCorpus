94addcd2a74b03e1bcc5a2758957053dcd4c683c
Heinrich <derhein@gmail.com>
2012-09-14 00:10:49 +0100

    Major improvements on bucket manager. Pretty well documented and stable now.

--- arxiv_bucket_manager.py
+++ arxiv_bucket_manager.py
@@ -1,40 +1,76 @@
 #
-# Automatic bucket extraction
+# Automatic Arxiv s3-bucket extraction management
+# 
+# Papers arrive as gz-files in the gz/ folder 
+# 
+# * extracts files into src/
+# * cleans up: 
+#      deletes images, restores file extensions
+#      deletes pdf and ps figures 
+# * merges tex and bbl files into one container
+# * sorts files:
+#   - *.tex into tex/ folder
+#   - *.bbl into bbl/ folder
+#   - *.pdf ...
+#   - *.ps  
+#           
+#
+#                        
+#  +------+   unzip   +-------+   sort   +------+
+#  |  gz  |  -------> |  src  | -------> | tex  |
+#  +------+           +-------+    |     +------+
+#     |                cleanup     +---> | bbl  |
+#     |                merge       |     +------+
+#     +----------------------------+---->| pdf? |
+#       filetype pdf                     +......+
 # 
 
+base_dir = '/home/heinrich/Desktop/related-work/arxiv_s3_buckets/'
 
-DEBUG = 1
-import ipdb
-breakpoint = ipdb.set_trace
+bucket_dir  = base_dir + '1001/'
 
+src_dir = base_dir + 'PROCESS/src/'
+tex_dir = base_dir + 'PROCESS/tex/'
+bbl_dir = base_dir + 'PROCESS/bbl/'
+pdf_dir = base_dir + 'PROCESS/pdf/'
 
-bucket_dir = '/home/heinrich/Desktop/related_work/arxiv_s3_buckets/1001/'
-gz_dir = bucket_dir + 'gz/'
-tmp_dir = bucket_dir + 'tmp/'
-src_dir = bucket_dir + 'src/'
 
 import tarfile, gzip, cStringIO, magic
-import os
-
+import os, shutil
 
 FileTypeDetector = magic.Magic()
+DEBUG = 0
 
-def extract_to_dir(filename , path='./' , prefix=''): 
-    """ Extracts the contents of a given gzipped file 
-    to a given path path adding a prefix to all extracted files.
+
+def bulk_extract_gz(source_dir='./',target_dir='./'):
+    ''' Extracts all gz files in source_dir to target_dir
+    adding a prefix to to extracted file prefix.
+    
     Example:
-    extract_to_dir('a.tar.gz','contents/','a_')
+    bulk_gz_extract('./','unzip/')
+    
+    Extract all files x.gz in ./ to unzip/x_(...)'''
 
-    Creates files a_xxx in the subdirectory 'contents/' for 
-    each file xxx in a.tar.gz
-    """
-    # The contents of the gz file can either be:
-    # - a tar archive which has not necessarily a proper extension
-    # - a source file
-    # We have to distinguish both cases
+    for filename in os.listdir(source_dir):
+        if not filename.endswith('.gz'): continue
+
+        if DEBUG: print 'Extracting', filename
+        extract_gz(source_dir + filename, 
+                   target_dir, 
+                   prefix = filename[:-3] + '_' # strip off ".gz" add "_"
+                   )
+        os.remove(source_dir + filename)
+            
+
+
+def extract_gz(file_path, target_dir='./' , prefix=''): 
+    '''Extracts a single gz file into the given path. 
+       The contents of the gz file can either be:
+       - a tar archive which has not necessarily a proper extension
+       - a source file'''
 
     # Unzip the file into memory:
-    fh = gzip.open(filename,'rb')
+    fh = gzip.open(file_path,'rb')
     file_string = fh.read()
     fh.close()
 
@@ -55,9 +91,9 @@ def extract_to_dir(filename , path='./' , prefix=''):
             name = curr_file.name
             name = name.replace('/','_').lower()    # strip subdirs, convert to lower case
 
-            if DEBUG: print 'from', filename, 'extracting', name, 'to', path + prefix + name
+#            if DEBUG: print 'from', file_path, 'extracting', name, 'to', target_dir + prefix + name
 
-            target = open(path + prefix + name, 'w')
+            target = open(target_dir + prefix + name, 'wb')
             target.write( content.read() )
             target.close()
 
@@ -66,136 +102,106 @@ def extract_to_dir(filename , path='./' , prefix=''):
 
     else:
         if DEBUG: print "Not a tar archiv! ", filetype
+        # Write file directly to target_dir
 
-        name = filename.split('/')[-1][:-3]
-
-        if filetype.startswith('LaTeX'):
-            name += '.tex'
-
-        target = open(path + prefix + name, 'w')
-        target.write( file_string )
+        name = file_path.split('/')[-1][:-3] # get filename from path
+        target = open(target_dir + prefix + '_no_tar_' + name, 'w')
+        target.write(file_string)
         target.close()
-    
 
-def bulk_gz_extract(source_path='./',target_path='./'):
-    ''' Extracts all gz files in source_path to target_path
-    adding a filename prefix.
-    
-    Example:
-    bulk_gz_extract('./','unzip/')
-    
-    Extract all files x.gz in ./ to unzip/x_(...)'''
 
-    for filename in os.listdir(source_path):
-        if not filename.endswith('.gz'): continue
+def magic_get_extension(file_path):
+    restore_dict = {
+        'LaTeX 2e document text'  : '.tex',
+        'LaTeX document text'     : '.tex',
+        'PostScript document text conforming DSC level 3.0, type EPS' : '.eps'
+        }
 
-        if DEBUG: print 'Extracting', filename
-        extract_to_dir(source_path + filename, target_path, prefix = filename[:-3] + '_')
+    file_type = FileTypeDetector.from_file(file_path)
+    if file_type in restore_dict: 
+        return restore_dict[file_type]
+    else:
+        return ""
 
 
 def clean_up_src(src_path):
     ''' 
     1) Delete Images, Styles
-    2) Move *.tex files to ../tex
-    3) Move *.bbl files to ../bbl
+    2) Restore File Extensions
     ''' 
 
-    RemoveExtensions = ['eps','png','jpg','gif','fig','eps3',       # Images
-                        'sty','cls','clo','bst','log','toc',        # Latex
-                        'cry','bak','sh'                            # Other Garbarge
+    remove_extensions = ['eps','png','jpg','gif','fig','eps3',       # Images
+                        'sty','cls','clo','bst','log','toc',         # Latex
+                        'cry','bak','sh'                             # Other Garbarge
                         ]
 
-    # Delete unnecessary files
-    for filename in os.listdir(src_path):
-        if True in [ filename.endswith(ex) for ex in RemoveExtensions ]:
-            if DEBUG: print 'removing', filename
-            os.remove(src_path + filename)
+    known_extensions  = ['pdf','ps','bbl','tex']
 
 
-    # Sort left over filenames by Arxiv Id's in dictionary. E.g.
-    # 
-    # file_by_ID['1001.1203'] = ['dawdaw.txt','awdaw.pdf','dwadw.tex']
-    #
-    file_by_ID = {}
-
-    for filename in os.listdir(src_path):
-        ID = filename[:9]
-        if not ID in file_by_ID:
-            file_by_ID[ID] = [filename]
-        else:
-            file_by_ID[ID].append(filename)
-
-
-    for ID, file_list in file_by_ID.items():
-        #
-        # Tex Files
-        #
-        # Concat all tex files belonging to a given ID 
-        # Write them to ../tex/ID.tex
-        #
-
-        tex_file_list = filter(lambda n: n.endswith('tex'), file_list )
-        if not tex_file_list == []: 
-
-            source_list = [ src_path + fname for fname in tex_file_list ]
-            target_file = src_path + '../tex/'+ID+'.tex'
-    
-            merge_files(source_list,target_file,remove_sources = True )
-
-
-        #
-        # same with BBL Files
-        # 
-        bbl_file_list = filter(lambda n: n.endswith('bbl'), file_list )
-        if not bbl_file_list == []: 
-            source_list = [ src_path + fname for fname in bbl_file_list ]
-            target_file = src_path + '../bbl/'+ID+'.bbl'
+    for file_name in os.listdir(src_path):
+        # Delete unnecessary files
+        if True in [ file_name.endswith(ex) for ex in remove_extensions ]:
+            if DEBUG: print 'removing', file_name
+            os.remove(src_path + file_name)
         
-            merge_files(source_list,target_file,remove_sources = True )
-
-        #
-        # PS Files
-        # 
-        # Discard if more than one
-        #
-
-        ps_file_list = filter(lambda n: n.endswith('ps'), file_list )
-        if not ps_file_list == []:
-
-            if len(ps_file_list) >= 2:
-                for file_name in ps_file_list:
-                    os.remove(src_path + file_name)
-
-        #
-        # PDF Files
-        # 
-        # Discard if more than one
-        #
-        pdf_file_list = filter(lambda n: n.endswith('pdf'), file_list )
-        if not pdf_file_list == []: 
-
-            if len(pdf_file_list) >= 2:
-                for file_name in pdf_file_list:
-                    os.remove(src_path + file_name)
+    for file_name in os.listdir(src_path):
+        # Use magic-lib to restore some file extensions of the REMAINING FILES
+        if not True in [ file_name.endswith(ex) for ex in known_extensions ]:
+            ext = magic_get_extension(src_path+file_name)
+            if DEBUG: print 'restoring', file_name, 'with', ext
+            os.rename(src_path + file_name, src_path + file_name + ext)
 
 
+def sort_files_away(src_dir, guide_book):
+    ''' Moves files from src_dir to directories specified in guide.
+        Syntax: guide = { '.tex': '/path/to/tex/file/store/', 
+                          '.xxx': '/path/to/xxx/file/store/'  }
+    '''
 
-def magic_ext_restore(path):
-    '''Uses magic lib to restore file extensions '''
+    for filename in os.listdir(src_dir):
+        for ext, target_dir in guide_book.items():
+            if filename.endswith(ext):
+                if DEBUG: print "Sorting ", filename, "to", target_dir
+                shutil.move(src_dir + filename, target_dir)
+
+
+def merge_by_extension(src_dir,ext_list):
+    ''' Merge all files in a given directory that have
+    the same identifyer and an extension in ext_list 
+    Example:
+
+    1001.1003_file_1.tex   \ 
+    1001.1003_file_2.tex    +==> 1001.1003.tex
+    1001.1003_file_3.tex   / 
+    1001.1004_file_1.tex    ==> 1001.1004.tex
+    '''
     
-    extensions={
-        'LaTeX 2e document text': '.tex',
-        'LaTeX document text'   : '.tex',
-        'PostScript document text conforming DSC level 3.0, type EPS' : '.eps'
-        }
-    
-    for file_name in os.listdir(path):
-        file_type = FileTypeDetector.from_file(path+file_name)
+    file_list = os.listdir(src_dir)
 
-        if file_type in extensions:
-            ext = extensions[file_type]
+    # Hash files by ID:
+    # file_by_ID['1001.1203'] = ['dawdaw.txt','awdaw.pdf','dwadw.tex']
+    file_with_id = {}
 
-            os.rename(path + file_name, path + file_name + ext )
+    for filename in file_list:
+        ID = filename[:9]
+        if not ID in file_with_id:
+            file_with_id[ID] = [filename]
+        else:
+            file_with_id[ID].append(filename)
+
+
+    for ID,files in file_with_id.items():
+        id_file_list = filter(lambda n: n.startswith(ID), file_list )
+
+        for ext in ext_list:
+            ext_file_list = filter(lambda n: n.endswith(ext), id_file_list )
+            if ext_file_list == []: continue
+
+            source_list = [ src_dir + name for name in ext_file_list ]
+            target_file = src_dir + ID + ext
+
+            merge_files(source_list, target_file, remove_sources = True )
+
 
 
 
@@ -219,16 +225,39 @@ def merge_files(source_list, target_file,
         for source_file in source_list:
             os.remove(source_file)
     
+def prepare_dirs(dir_list):
+    for directory in dir_list:
+        if not os.path.exists(directory):
+            if DEBUG: print 'creating directory', directory
+            os.makedirs(directory)
 
 
 if __name__ == '__main__':    
+
+    DEBUG = 1
+    import ipdb
+    breakpoint = ipdb.set_trace
+
     try:
-        DEBUG = 0
-#        bulk_gz_extract(gz_dir,src_dir)
-        DEBUG = 1
+        # Create dirs if not already there
+        prepare_dirs([src_dir,tex_dir,pdf_dir,bbl_dir,tex_dir])
+
+        # Take pdf files out of the way
+        sort_files_away(bucket_dir, guide_book={ 'pdf': pdf_dir })
+
+        # 1. Extracting
+        bulk_extract_gz(bucket_dir, src_dir)
+        
+        # 2. Cleaning
         clean_up_src(src_dir)
-        magic_ext_restore(src_dir)
-        clean_up_src(src_dir)
+        merge_by_extension(src_dir,['.tex','.bbl'])
+
+        # 3. Sorting
+        sort_files_away(src_dir, guide_book={
+                'tex': tex_dir,
+                'bbl': bbl_dir
+                })
+
 
     except:
         print "EROOR"
