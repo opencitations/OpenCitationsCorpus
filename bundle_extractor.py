0e48ebbedc8fce7934e091dcae7d696c87238ba5
Heinrich Hartmann <heinrich@heinrich-hartmann.loc>
2012-09-13 01:35:28 +0100

    Nothing important yet. Just setting up EGit.

---
+++ bundle_extractor.py
@@ -0,0 +1,15 @@
+#
+# Process the bundleded arxiv dumps:
+# 
+# PDF Dumps:
+# 
+# 1) pdf -> txt
+# 2) Extract refs using pdf-extract -> xml file
+#
+#
+# SRC Dumps:
+# 
+# 1) gz -> Contain a wealth of other formats  pdf, tex, bbl, 
+#
+# 
+# 
