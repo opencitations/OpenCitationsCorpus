4cec819fcfd1ff0c405811b9c5d8da6c8491e7c8
Heinrich <derhein@gmail.com>
2012-09-30 19:21:56 +0100

    Improved web Demo * Caching support * Sorting by citation count * Nice author pages * Author abbreviations * Improved structure

--- DemoApp/demo_app_neo4j.py
+++ DemoApp/demo_app_neo4j.py
@@ -1,4 +1,5 @@
 from os import curdir, sep
+from re import findall
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from neo4j import GraphDatabase
 from unicodedata import normalize
@@ -7,6 +8,31 @@ db = GraphDatabase('db_folder')
 
 import ipdb
 BREAK=ipdb.set_trace
+
+
+
+def main():
+    try:
+        init_globals()
+        server = HTTPServer(('', 8000), MyHandler)
+        print 'Welcome to the machine...'
+        server.serve_forever()
+    except:
+        print '^C received, shutting down server'
+        db.shutdown()
+        server.socket.close()
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            self.send_response(200)
+            self.send_header('Content-type',	'text/html')
+            self.end_headers()
+            self.wfile.write(get_html(self.path))
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
 
 
 def init_globals(db=db):
@@ -23,9 +49,6 @@ def init_globals(db=db):
     ROOT   = db.reference_node
 
 
-
-
-
 def get_html(path):
     if path.endswith('.ico'): return ''
     if path.split('/')[-2] == 'author':
@@ -39,89 +62,74 @@ def get_html(path):
     else:
         return "Id %s not found" % RW_ID
 
-    ref_string = '<ul>'
-    reference_count = 0
-    for ref in paper_node.ref.outgoing:
-        reference_count += 1
-        sub_paper_node = ref.endNode
-        sub_citation_count = 0
-        for sub_ref in sub_paper_node.ref.incoming: sub_citation_count += 1
-        authors = ' and '.join([ rel.endNode['name'] for rel in sub_paper_node.author ])
-        title   = sub_paper_node['title']
-        year    = sub_paper_node['date'][0:4]
-        entry   = u"{authors}, {title}, {year}".format(**locals())
-        href = '/' + sub_paper_node['source_id'].replace('/','_')
-        ref_string += to_ascii(u'<li><a href="{href}">({sub_citation_count}) {entry} </a></li> \n'.format(**locals()))
 
-    ref_string += '\n'.join(['<li>' + u_ref_string + '</li>' for u_ref_string in paper_node['unknown_references'][1:]])
+    ref_list = [ (get_ref_html(ref.endNode),ref.endNode['c_citation_count']) for ref in paper_node.ref.outgoing ]
+    # sort by cite rank
+    ref_list = sorted( ref_list, key = lambda x: x[1], reverse=True)
+    ref_string = '<ul>'
+    ref_string += '\n'.join( entry[0] for entry in ref_list )
+    ref_string += '\n'.join( '<li>' + u_ref_string + '</li>' for u_ref_string in paper_node['unknown_references'][1:] )
     ref_string += '</ul>'
 
-    citation_count = 0
+    cite_list = [ (get_ref_html(ref.startNode),ref.startNode['c_citation_count']) for ref in paper_node.ref.incoming ]
+    # sort by cite rank
+    cite_list = sorted( cite_list, key = lambda x: x[1], reverse=True)
     cite_string = '<ul>'
-    for ref in paper_node.ref.incoming:
-        citation_count += 1
-        sub_paper_node = ref.startNode
-        sub_citation_count = 0
-        for sub_ref in sub_paper_node.ref.incoming: sub_citation_count += 1
-        authors = ' and '.join([ author_rel.endNode['name'] for author_rel in sub_paper_node.author ])
-        title   = sub_paper_node['title']
-        year    = sub_paper_node['date'][0:4]
-        entry   = u"{authors}, {title}, {year}".format(**locals())
-        href = '/' + sub_paper_node['source_id'].replace('/','_')
-        cite_string += to_ascii(u'<li><a href="{href}">({sub_citation_count}) {entry} </a></li>'.format(**locals()))
+    cite_string += '\n'.join([ entry[0] for entry in cite_list ])
     cite_string += '</ul>'
 
-
-
-    html = u'''
-<body class="tex2jax_ignore">
-<div id='headder'>
-    <h1 style='display:inline'> <a href='http://related-work.net'>Related-Work.net</a> </h1> 
-    <h2 style='display:inline'> 
-      >
-      <a href='http://arxiv.org'> arXiv </a>  
-      >
-      <a href='{source_url}'>{short_id}</a>
-    </h2>
-</div>
-<div align='center' id='body'>
-    <div id='meta'>
-         <h1 class='tex2jax_process'>{titel}</h1>
-         <h2>by: {author}</h2>
-         <div id='abstract' class='tex2jax_process'>
-             <h2>Abstract:</h2> {abstract} 
-         </div>
-         <div>
-          <div style='float:left; width:100%' align='left' class='references'>
-              <h2>Cited by ({citation_count})</h2>
-              {citations}
-         </div>
-         <div style='float: left; width:100%;' align='left' class='references'>
-              <h2>References ({reference_count})</h2>
-              {references}
-         </div>
-         <div>
-
-    </div>
-
-
-</div>
-</body>
-'''.format(
-        node_id = RW_ID,
-        short_id = RW_ID.split(':')[-1],
-        source_url = paper_node['source_url'],
-        titel = paper_node['title'],
-        abstract = paper_node['abstract'],
-        author =  ' and '.join([ '<a href= "%s"> %s </a>' % ("/author/" + author_rel.endNode['name'].replace(" ","_"), author_rel.endNode['name']) for author_rel in paper_node.author ]),
-        references = ref_string,
-        citations = cite_string,
-        citation_count = citation_count,
-        reference_count = reference_count
+    html = html_template.format(
+        node_id         = RW_ID,
+        short_id        = RW_ID.split(':')[-1],
+        source_url      = paper_node['source_url'],
+        titel           = paper_node['title'],
+        abstract        = paper_node['abstract'],
+        author          = get_author_html(paper_node),
+        references      = ref_string,
+        citations       = cite_string,
+        citation_count  = paper_node['c_citation_count'],
+        reference_count = paper_node['c_reference_count'],
         )
-
         
-    return to_ascii(mathjax + css + html)
+    return to_ascii(css + html)
+
+def get_author_html(paper_node):
+    authors = paper_node['c_authors'].split(" and ")
+    authors.sort()
+    return ' and '.join(
+        '''<a href="{href}"> {name} </a>'''.format(
+            href = '/author/'+name.replace(' ','_'),
+            name = abbrev_full_name(name))
+        for name in authors)
+
+
+def get_ref_html(node):
+    return to_ascii(u'''
+           <li><a href="{href}">{authors}, {title}, {year} (citations: {citation_count})</a></li> \n
+    '''.format(
+            href           = '/' + node['source_id'].replace('/','_'),
+            authors        = get_author_names(node),
+            title          = node['title'],
+            year           = node['date'][0:4],
+            citation_count = node['c_citation_count'],
+            cite_rank      = node['c_cite_rank']
+            ))
+
+def get_author_names(paper_node):
+    authors = paper_node['c_authors'].split(" and ")
+    authors.sort()
+    authors = [ abbrev_full_name(name)  for name in authors ]
+    html = ' and '.join(authors[:3])
+    if len(authors) > 3:
+        html += ' et.al.'
+    return html
+
+
+def abbrev_full_name(name):
+    sir_name = name.split(', ')[0]
+    rest     = ','.join(name.split(', ')[1:])
+    abbrev_first_name = ' '.join( " " + first_name[0] +'.' for first_name in findall(r"[a-zA-Z]+",rest))
+    return sir_name + ' ' + abbrev_first_name
 
 
 def to_ascii(string):
@@ -130,8 +138,6 @@ def to_ascii(string):
     except TypeError:
         out= string
     return out
-    
-
 
 
 def get_autor_page(author_name):
@@ -139,85 +145,23 @@ def get_autor_page(author_name):
     for author_node in author_idx['name'][author_name]: break
     else: return "Author %s not found" % author_name
 
-    paper_string = '<ul>'
-    for author_rel in author_node.author.incoming:
-        paper_node = author_rel.startNode
-        href = '../'+paper_node['source_id'].replace('/','_')
-        title = paper_node['title']
-        year = paper_node['date'][0:4]
-        paper_string += u'<li><a href="{href}"> {title}, {year}</li>'.format(**locals())
-    paper_string += '</ul>'
-        
-    html = u'''
-<h1> {author_name} </h1>
-<h2> Articles </h2>
-{paper_string}
-'''.format(author_name = author_name, paper_string = paper_string)
+    ref_list = [ (get_ref_html(ref.startNode),ref.startNode['c_citation_count']) for ref in author_node.author.incoming ]
+    # sort by date
+    ref_list = sorted( ref_list, key = lambda x: x[1], reverse=True)
+    ref_string = '<ul>'
+    ref_string += '\n'.join( entry[0] for entry in ref_list )
+    ref_string += '</ul>'
 
-    return to_ascii(html)
+    html = author_template.format(
+        author_name = author_name,
+        author_url  = '#',
+        references = ref_string,
+        )       
+
+    return to_ascii(css + html)
 
 
 
-
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        try:
-            self.send_response(200)
-            self.send_header('Content-type',	'text/html')
-            self.end_headers()
-            self.wfile.write(get_html(self.path))
-        except IOError:
-            self.send_error(404,'File Not Found: %s' % self.path)
-
-def main():
-    try:
-        init_globals()
-        server = HTTPServer(('', 8000), MyHandler)
-        print 'Welcome to the machine...'
-        server.serve_forever()
-    except:
-        print '^C received, shutting down server'
-        db.shutdown()
-        server.socket.close()
-
-
-
-
-def get_meta_dict(arxiv_id):
-    con = lite.connect(meta_db)
-
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM meta WHERE arxiv_id = '%s'" % arxiv_id)
-        row = cur.fetchone()
-        if row is None: return {}
-        BREAK()
-        row = map(to_ascii, row)
-        schema = 'rec_id, authors, title, abstract, info, subject, year'.split(', ')
-        author_dict = dict(zip(schema,row))
-        
-    return author_dict
-
-
-def get_references(arxiv_id):
-    con = lite.connect(meta_db)
-
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT target_id FROM refs WHERE source_id = '%s'" % arxiv_id)
-        references = [x[0].replace('/','_') for x in cur.fetchall()]
-    return map(to_ascii,references)
-
-
-def get_citations(arxiv_id):
-    con = lite.connect(meta_db)
-
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT source_id FROM refs WHERE target_id = '%s'" % arxiv_id)
-        rows = cur.fetchall()
-        citations = [x[0].replace('/','_') for x in rows]
-    return map(to_ascii, citations)
 
 
 
@@ -253,6 +197,7 @@ font-size: x-large;
 font-weight: bold;
 line-height: 150%;
 }
+
 h2 {
 font-family: 'Lucida Grande', helvetica, arial, verdana, sans-serif;
 font-size: medium;
@@ -270,14 +215,14 @@ width: 100%;
 text-align:justify;
 }
 
-#abstract h2 {
+h2.abstract {
 display: inline;
 line-height: 100%;
 }
 
 a:link { color:#000070; text-decoration:none; }
 a:hover { text-decoration:underline; }
-a:visited { color:#A00093; text-decoration:none; }
+a:visited { color:#551A8B; text-decoration:none; }
 
 
 #meta {
@@ -290,13 +235,16 @@ a:visited { color:#A00093; text-decoration:none; }
 padding: 8px;
 border-style: none none dotted none;
 border-width: 2px;
-background-color: #E9E6EE;
+background-color: #EEEEEE;
 }
 
 .references {
 font-family: 'Lucida Grande', helvetica, arial, verdana, sans-serif;
 font-size: 90%;
 line-height: 130%;
+text-align:left;
+float:left; 
+width:100%;
 } 
 
 .references ul {
@@ -310,6 +258,66 @@ margin-top: 5;
 </style>'''
 
 
+
+author_template = u'''
+<body>
+<div id='headder'>
+    <h1 style='display:inline'> <a href='http://related-work.net'>Related-Work.net</a> </h1> 
+    <h2 style='display:inline'>
+      >
+      author
+      >
+      <a href='{author_url}'> {author_name} </a>
+    </h2>
+</div>
+<div align='center' id='body'>
+    <div id='meta'>
+         <h1 class='tex2jax_process'>{author_name}</h1>
+         <div class='references'>
+              <h2>Articles:</h2>
+              {references}
+         </div>
+    </div>
+</div>
+</body>
+'''
+
+
+html_template = u'''
+<body class="tex2jax_ignore">
+<div id='headder'>
+    <h1 style='display:inline'> <a href='http://related-work.net'>Related-Work.net</a> </h1> 
+    <h2 style='display:inline'> 
+      >
+      arxiv
+      >
+      <a href='#'>{short_id}</a>
+    </h2>
+    
+    <h2 style='display:inline; float:right; line-height:100%'><a href='{source_url}'> source </a></h2>
+</div>
+<div align='center' id='body'>
+    <div id='meta'>
+         <h1 class='tex2jax_process'>{titel}</h1>
+         <h2>by: {author}</h2>
+         <div id='abstract' class='tex2jax_process'>
+             <h2 class='abstract'>Abstract:</h2> {abstract} 
+         </div>
+         <div>
+          <div class='references'>
+              <h2>Cited by ({citation_count})</h2>
+              {citations}
+         </div>
+         <div class='references'>
+              <h2>References ({reference_count})</h2>
+              {references}
+         </div>
+         <div>
+
+    </div>
+</div>
+</body>
+'''
 
 
 
