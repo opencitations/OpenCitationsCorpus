8c7ee9a7dad92e2693fb170660b7c22cc26c0a1b
Martyn Whitwell <martyn.whitwell@gmail.com>
2013-02-12 00:19:30 +0000

    working on pubmedcentral OA importer; largely based on Mark's parser, but written for lxml.etree

--- Importer/OpenCitationsImportLibrary.py
+++ Importer/OpenCitationsImportLibrary.py
@@ -17,40 +17,155 @@ from oaipmh.metadata import MetadataRegistry, MetadataReader#, oai_dc_reader
 from lxml import etree
 from lxml.etree import SubElement
 from oaipmh import common
+import Parse
+
 
 class MetadataReaderPMCFM(object):
+
+    _namespaces={
+        'nlmaa': 'http://dtd.nlm.nih.gov/2.0/xsd/archivearticle',
+        'xlink': 'http://www.w3.org/1999/xlink',
+        'mml' : 'http://www.w3.org/1998/Math/MathML',
+        'xsi': 'http://www.w3.org/2001/XMLSchema-instance'}
+
     """A metadata reader for PubMedCentral Front Matter data.
     """
-    def __init__(self, fields, namespaces=None):
-        self._fields = fields
-        self._namespaces = namespaces or {}
+    def __init__(self):
+        pass
 
-    def __call__(self, element):
+    def set_map_with_element_text(self, map, key, element, xpath):
+        if element is not None:
+            sought_element = element.find(xpath, self._namespaces)
+            if sought_element is not None and hasattr(sought_element,'text'):
+                map[key] = sought_element.text
+                return True
+        return False
+
+    def set_map_with_element_xml(self, map, key, element, xpath):
+        if element is not None:
+            sought_element = element.find(xpath, self._namespaces)
+            if sought_element is not None:
+                map[key] = etree.tostring(sought_element)
+                return True
+        return False
+        
+        
+
+    def __call__(self, metadata_element):
         map = {}
 
-        # create XPathEvaluator for this element
-        xpath_evaluator = etree.XPathEvaluator(element, 
-                                               namespaces=self._namespaces)
+        print "XML ELEMENT---------"
+        print(etree.tostring(metadata_element, pretty_print=True))
+
+        # front
+        front = metadata_element.find("nlmaa:article/nlmaa:front", self._namespaces)
+
+        # journal meta
+        journal_meta = front.find("nlmaa:journal-meta", self._namespaces)
         
-        e = xpath_evaluator.evaluate
-        # now extra field info according to xpath expr
-        for field_name, (field_type, expr) in self._fields.items():
-            if field_type == 'bytes':
-                value = str(e(expr))
-            elif field_type == 'bytesList':
-                value = [str(item) for item in e(expr)]
-            elif field_type == 'text':
-                # make sure we get back unicode strings instead
-                # of lxml.etree._ElementUnicodeResult objects.
-                value = unicode(e(expr))
-            elif field_type == 'textList':
-                # make sure we get back unicode strings instead
-                # of lxml.etree._ElementUnicodeResult objects.
-                value = [unicode(v) for v in e(expr)]
-            else:
-                raise Error, "Unknown field type: %s" % field_type
-            map[field_name] = value
+        # article metadata
+        article_meta = front.find("nlmaa:article-meta", self._namespaces)
+        
+        if journal_meta is not None:
+            
+            map["journal"] = {}
+
+            (self.set_map_with_element_text(map["journal"], "name", journal_meta, "nlmaa:journal-title-group/nlmaa:journal-title") or
+                self.set_map_with_element_text(map["journal"], "name", journal_meta, "nlmaa:journal-title"))
+            
+            issns = journal_meta.findall('nlmaa:issn', self._namespaces)
+            if issns:
+                map["journal"]["identifier"] = []
+                for issn in issns:
+                    map["journal"]["identifier"].append({"type": issn.get('pub-type'), "id": issn.text})
+            
+            self.set_map_with_element_text(map["journal"], "publisher", journal_meta, "nlmaa:publisher/nlmaa:publisher-name")
+        
+        if article_meta is not None:
+    
+            #identifiers
+            article_ids = article_meta.findall("nlmaa:article-id", self._namespaces)
+            if article_ids:
+                map["identifier"] = []
+                for article_id in article_ids:
+                    map["identifier"].append({"type": article_id.get('pub-id-type'), "id": article_id.text})
+            
+            #title
+            self.set_map_with_element_text(map, "title", article_meta, "nlmaa:title-group/nlmaa:article-title")
+            
+            #pagination
+            self.set_map_with_element_text(map, "volume", article_meta, "nlmaa:volume")
+            self.set_map_with_element_text(map, "issue", article_meta, "nlmaa:issue")
+            self.set_map_with_element_text(map, "firstpage", article_meta, "nlmaa:fpage")
+            self.set_map_with_element_text(map, "lastpage", article_meta, "nlmaa:lpage")
+            if "firstpage" in map:
+                if "lastpage" in map and (map["firstpage"] != map["lastpage"]):
+                    map["pages"] = map["firstpage"] + "-" + map["lastpage"]
+                else:
+                    map["pages"] = map["firstpage"]
+
+            #publication date
+            # why only use the pmc-release date? need to check with Mark
+            pub_date = article_meta.find("nlmaa:pub-date[@pub-type='pmc-release']", self._namespaces)
+            if pub_date is not None:
+                self.set_map_with_element_text(map, "year", pub_date, "nlmaa:year")
+                self.set_map_with_element_text(map, "month", pub_date, "nlmaa:month")
+                self.set_map_with_element_text(map, "day", pub_date, "nlmaa:day")
+            
+            #copyright
+            self.set_map_with_element_text(map, "copyright", article_meta, "nlmaa:permissions/nlmaa:copyright-statement")
+            
+            #abstract
+            self.set_map_with_element_xml(map, "abstract", article_meta, "nlmaa:abstract")
+            
+            #keywords
+            keywords = article_meta.findall("nlmaa:kwd_group/nlmaa:kwd", self._namespaces)
+            if keywords:
+                map["keyword"] = []
+                for keyword in keywords:
+                    map["keyword"].append(keyword.text)
+            
+            #contributors
+            contribs = article_meta.findall("nlmaa:contrib-group/nlmaa:contrib", self._namespaces)
+            
+            if contribs:
+                map["author"] = []
+                map["editor"] = []
+                for contrib in contribs:
+                    entity = {}
+                    if contrib.get('corresp') == 'yes':
+                        entity["corresponding"] = 'yes'
+                    self.set_map_with_element_text(entity, "lastname", contrib, "nlmaa:name/nlmaa:surname")
+                    self.set_map_with_element_text(entity, "firstname", contrib, "nlmaa:name/nlmaa:given-names")
+                    if "lastname" in entity and "firstname" in entity:
+                        entity["name"] = entity["lastname"] + " " + entity["firstname"]
+                    email = contrib.find("nlmaa:address/nlmaa:email", self._namespaces)
+                    if email is None:
+                        email = contrib.find("nlmaa:email", self._namespaces)
+                    if email is not None:
+                        entity["identifier"] = {"type": "email", "id": email.text}
+                    
+                    xrefs = contrib.findall("nlmaa:xref", self._namespaces)
+                    affs = article_meta.findall("nlmaa:aff", self._namespaces) #NOT ContribGroup - check with Mark
+                    for xref in xrefs:
+                        if xref.get('ref-type') == "aff":
+                            rid = xref.get("rid")
+                            for aff in affs:
+                                if aff.get("id") == rid:
+                                    if "affiliation" not in entity:
+                                        entity["affiliation"] = []
+                                    for text in aff.itertext():
+                                        entity["affiliation"].append(text)
+                                    
+                    if contrib.get("contrib-type") == "author":
+                        map["author"].append(entity)
+                    if contrib.get("contrib-type") == "editor":
+                        map["editor"].append(entity)
+
+
         return common.Metadata(map)
+
+
 
 
 
@@ -83,19 +198,8 @@ class OAIImporter:
         'dc' : 'http://purl.org/dc/elements/1.1/'}
         )
 
-    METADATA_READER_PMC_FM = MetadataReaderPMCFM(
-        fields={
-        'title':       ('text', 'pmc:article/pmc:front/pmc:article-meta/pmc:title-group/pmc:article-title/text()'),
-        'contrib' :    ('textList', 'pmc:article/pmc:front/pmc:article-meta/pmc:contrib-group/pmc:contrib/pmc:name/pmc:surname/text()'),
+    METADATA_READER_PMC_FM = MetadataReaderPMCFM()
 
-        'doi':       ('text', 'pmc:article/pmc:front/pmc:article-meta/pmc:article-id[@pub-id-type="doi"]/text()')
-        },
-        namespaces={
-        'pmc': 'http://dtd.nlm.nih.gov/2.0/xsd/archivearticle',
-        'xlink': 'http://www.w3.org/1999/xlink',
-        'mml' : 'http://www.w3.org/1998/Math/MathML',
-        'xsi': 'http://www.w3.org/2001/XMLSchema-instance'}
-        )
 
     def run(self):
         print "Importing from: %s" % self.uri
