24ec44c7865b9c598830afc18ca3374c3c4eba8d
Martyn Whitwell <martyn.whitwell@gmail.com>
2013-02-27 12:22:25 +0000

    Added bucket extractor

--- Importer/Downloader.py
+++ Importer/Downloader.py
@@ -15,9 +15,14 @@ class DownloadArXiv(object):
     def __init__(self):
         # We change the directory later on. Therefore all paths have to be absolute here.
         self.cur_dir = os.getcwd()
-        self.contents_file = self.cur_dir + '/DATA/arXiv/arxiv_s3_downloads.txt'
+        self.contents_file = self.cur_dir + '/DATA/arXiv/arXiv_s3_downloads.txt'
+        self.extraction_queue = self.cur_dir + '/DATA/arXiv/arXiv_extraction_queue.txt'
         self.s3_cmd_ex     = self.cur_dir + "/../ImportArxiv/tools/s3cmd/s3cmd" #TODO: Tidy this up
         self.dl_dir        = self.cur_dir + '/DATA/arXiv/downloads/'
+        self.extract_dir =  self.cur_dir + '/DATA/arXiv/sources/'
+        self.tmp_dir = self.cur_dir + '/DATA/arXiv/tmp/'
+
+        #self.bucket_dir = '../DATA/BUCKETS/'
 
 
     def download(self):
@@ -45,3 +50,42 @@ class DownloadArXiv(object):
             if 'x' in nbRawInput('',timeout=1):
                 print "Download suspended. Restart script to resume."
                 break
+
+
+    def extract(self):
+        print 'Press "x" to break'
+        if not os.path.exists(self.tmp_dir):
+            os.mkdir(self.tmp_dir)
+
+        if not os.path.exists(self.extract_dir):
+            os.mkdir(self.extract_dir)
+
+        #Creates arXiv_extraction_queue.txt if it doesn't exist by finding all the tar files in the download folder
+        if not os.path.exists(self.extraction_queue):
+            call('find {source_dir}*.tar -type f > {target_file}'.format(
+                    source_dir = self.dl_dir,
+                    target_file = self.extraction_queue 
+                    ) , shell = True)
+
+        while True:
+            file_name = fq.get(self.extraction_queue)
+            if file_name is None: break
+
+            print "Extracting bucket" , file_name
+            if call(['tar','xf',file_name,'-C',self.tmp_dir]):
+                # call returns 1 on error.
+                break
+
+            if call('find %s -name *.gz -type f -exec mv {} %s \;' % (self.tmp_dir, self.extract_dir), shell = True):
+                break
+
+            if call('rm -R ' + self.tmp_dir + '*', shell=True):
+                break
+
+            fq.pop(self.extraction_queue)
+
+            # break if x was pressed
+            if nbRawInput('',timeout=1) == 'x':
+                print "Extraction suspended. Restart script to resume."
+                break
+
--- Importer/Runner.py
+++ Importer/Runner.py
@@ -24,7 +24,7 @@ def main():
 
 
     d = Downloader.DownloadArXiv()
-    d.download()
+    d.extract()
 
 
     print "Finished."
