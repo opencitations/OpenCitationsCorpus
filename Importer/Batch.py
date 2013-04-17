9295ffb8bbf6a7d8ad8750e5ea9a6574d635494e
Martyn Whitwell <martyn.whitwell@gmail.com>
2013-04-17 14:56:32 +0100

    remove logging from batch, reset batch size to 5000

--- Importer/Batch.py
+++ Importer/Batch.py
@@ -1,15 +1,6 @@
 import requests, json
 import Config
 from Matcher import Matcher
-import logging
-import datetime
-import os
-
-if not os.path.exists('./log/'):
-    os.mkdir('./log/')
-
-now = datetime.datetime.now()
-logging.basicConfig(filename=now.strftime('./log/batch-%Y-%m-%d.log'),level=logging.DEBUG)
 
 # a batch class for managing batches of records to send to ES, that also does the bulk loading
 class Batch(object):
@@ -25,17 +16,23 @@ class Batch(object):
         if self.temp:
             self._es_bulk_load()
 
+   
     def _es_bulk_load(self):
         print "sending batch of " + str(len(self.temp))
         # http://www.elasticsearch.org/guide/reference/api/bulk.html
         data = ''
         for r in self.temp:
-            print(r)
-            print(json.dumps( r ))
+            #print("\n\nRAW DATA")
+            #print(r)
+            #print("\n\n To CONVERT")
+            #print(self.convert(r))
+            #print("\nJSON")
+            #print(json.dumps( r ))
+
             data += json.dumps( {'index':{'_id': r['_id']}} ) + '\n'
             data += json.dumps( r ) + '\n'
         self.temp = []
-        logging.debug(data)
+
         r = requests.post(Config.elasticsearch['uri_records'] + '_bulk', data=data)
 
         # if matching is enabled, then try to match whatever was in the batch to the rest of the index content
