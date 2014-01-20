fd4093b0df38405cc934c7ca320dc625d2cef9e3
Heinrich Hartmann <hartmann@uni-koblenz.de>
2014-01-20 15:21:42 +0100

    Improved get_pmc_oa.sh script

--- get_pmc_oa.sh
+++ get_pmc_oa.sh
@@ -1,12 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
-cd Importer
-mkdir pmcoa
-cd pmcoa
+# stop script if something goes wrong
+set -e
+
+mkdir -p Importer/pmcoa
+cd Importer/pmcoa
 
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/articles.A-B.tar.gz
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/articles.C-H.tar.gz
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/articles.I-N.tar.gz
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/articles.O-Z.tar.gz
 
-cd ../../
